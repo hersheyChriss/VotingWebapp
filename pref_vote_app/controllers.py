a7e72a18c5af5306cbbeff755543f6afe6943cdd
jmhossler <john.m.hossler@gmail.com>
2017-09-14 14:50:03 -0500

    Added a couple things

--- pref_vote_app/controllers.py
+++ pref_vote_app/controllers.py
@@ -12,10 +12,27 @@ place_holder = None
 polls = {}
 
 
+open_polls  = set()
+
+
 @app.route('/api/submit_vote', methods=['POST'])
 def submit_vote():
     data = json.loads(request.data)
     vote_info = {int(key): data[u'preferences'][key] for key in data[u'preferences']}
     poll_id = data[u'poll_id']
     polls[poll_id].submit_ballot(place_holder.Ballot(vote_info))
+    return 'OK', 200
+
+
+@app.route('/api/check_status', methods=['POST'])
+def check_status():
+    data = json.loads(request.data)
+    poll_id = data[u'poll_id']
+    return json.dumps(polls[poll_id].check_status()), 200
+
+
+@app.route('/api/start_poll', methods=['POST'])
+def start_poll():
+    data = json.loads(request.data)
+    open_polls.add(data[u'poll_id'])
     return 'OK', 200
--- test/unit/test_flask_api.py
+++ test/unit/test_flask_api.py
@@ -20,3 +20,22 @@ class TestFlaskApiSuite(object):
         stv_mock.Ballot.assert_called_once_with({0: 'bar',1:'baz'})
         poll.submit_ballot.assert_called_once_with(stv_mock.Ballot.return_value)
         assert resp.status_code == 200
+        assert resp.data == 'OK'
+
+    @patch('pref_vote_app.controllers.polls')
+    def test_check_status(self, polls_mock):
+        app = cntrlr.app.test_client()
+        poll = polls_mock.__getitem__.return_value
+        poll.check_status.return_value = {'foo': 'bar'}
+        resp = app.post('/api/check_status', data=json.dumps(dict(poll_id='foo')))
+        polls_mock.__getitem__.assert_called_once_with('foo')
+        poll.check_status.assert_called_once_with()
+        assert resp.status_code == 200
+        assert resp.data == json.dumps(poll.check_status.return_value)
+
+    def test_start_poll(self):
+        app = cntrlr.app.test_client()
+        resp = app.post('/api/start_poll', data=json.dumps(dict(poll_id='foo')))
+        assert 'foo' in cntrlr.open_polls
+        assert resp.status_code == 200
+        assert resp.data == 'OK'
