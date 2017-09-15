209674802c57e74ec3ecc689efc10ed087e1cd7c
Chris <christopher.hershey719@gmial.com>
2017-09-14 19:58:23 -0500

    Final changes before meetup

--- webapp/src/Routes.js
+++ webapp/src/Routes.js
@@ -12,10 +12,9 @@ export default class Routes extends Component {
                 <Switch>
                 	<Route exact path="/" component={Create} />
                     <Route exact path="/create" component={Create} />
-                	<Route exact path="/:pin" component={Create} />
                     <Route exact path="/create/:pin" component={Create} />
                     <Route exact path="/poll/:id" component={Poll} />
-                    <Route exact path="/results:id" component={Results} />
+                    <Route exact path="/results/:id" component={Results} />
                 </Switch>
             </BrowserRouter>
         );
